e359745f41d2a6f3f7c51d97cb49f6c874fb6665
Jayqueue1 <ewurajoy@yahoo.com>
2020-10-12 00:21:32 +0000

    Initial Commit

---
+++ src/Home.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+function ShowHome(){
+return(
+ 
+
+
+<>
+<div class="image"> 
+
+
+<h1>Welcome To Weather Pro</h1>
+<nav>
+<ul>
+<li><a href="Home">Home</a></li>
+<li><a href="Login">Login</a></li>
+
+</ul>
+</nav> 
+</div>  
+  
+     </>
+);
+
+}
+export default ShowHome;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ShowHome from './Home';
 
 ReactDOM.render(
   <React.StrictMode>
